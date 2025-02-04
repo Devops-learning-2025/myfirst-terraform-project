c3443de1afced3b1fd7bc4b08821f1debaf72e64
suvarna <suvarna.akkala@gmail.com>
2025-02-04 12:20:34 +0100

    v1 of ec2 file

---
+++ Resources/EC2.tf
@@ -0,0 +1,24 @@
+ # Specify Terraform provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure AWS Provider
+provider "aws" {
+  region = "us-east-1"  
+}
+
+# Create an EC2 instance
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-038ff3475cbb62351"  # Xenial Xerus ubuntu
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyFirstTerraformEC2"
+  }
+}
